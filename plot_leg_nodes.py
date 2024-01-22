a229af7f82faecb53dd3e7068ad62a8d82c19aed
Bakes, Sam <sam.bakes@ukaea.uk>
2024-01-22 14:35:28 +0000

    python files for plotting first set of results

---
+++ plot_leg_nodes.py
@@ -0,0 +1,458 @@
+#!/usr/bin/env python
+import numpy as N
+import pylab
+def main():
+ r =          2.4733
+ z =          6.3755
+ pylab.plot(r,z, "bx")
+ r =          2.5617
+ z =          6.3810
+ pylab.plot(r,z, "bx")
+ r =          2.6443
+ z =          6.4111
+ pylab.plot(r,z, "bx")
+ r =          2.7201
+ z =          6.4561
+ pylab.plot(r,z, "bx")
+ r =          2.7905
+ z =          6.5082
+ pylab.plot(r,z, "bx")
+ r =          2.8574
+ z =          6.5639
+ pylab.plot(r,z, "bx")
+ r =          2.9224
+ z =          6.6221
+ pylab.plot(r,z, "bx")
+ r =          2.9860
+ z =          6.6823
+ pylab.plot(r,z, "bx")
+ r =          3.0484
+ z =          6.7433
+ pylab.plot(r,z, "bx")
+ r =          3.1097
+ z =          6.8052
+ pylab.plot(r,z, "bx")
+ r =          3.1702
+ z =          6.8679
+ pylab.plot(r,z, "bx")
+ r =          3.2298
+ z =          6.9312
+ pylab.plot(r,z, "bx")
+ r =          3.2888
+ z =          6.9952
+ pylab.plot(r,z, "bx")
+ r =          3.3472
+ z =          7.0598
+ pylab.plot(r,z, "bx")
+ r =          3.4048
+ z =          7.1249
+ pylab.plot(r,z, "bx")
+ r =          3.4620
+ z =          7.1906
+ pylab.plot(r,z, "bx")
+ r =          3.5188
+ z =          7.2568
+ pylab.plot(r,z, "bx")
+ r =          3.5749
+ z =          7.3233
+ pylab.plot(r,z, "bx")
+ r =          3.6307
+ z =          7.3903
+ pylab.plot(r,z, "bx")
+ r =          3.6862
+ z =          7.4576
+ pylab.plot(r,z, "bx")
+ r =          3.7415
+ z =          7.5252
+ pylab.plot(r,z, "bx")
+ r =          3.7970
+ z =          7.5932
+ pylab.plot(r,z, "bx")
+ r =          3.8533
+ z =          7.6622
+ pylab.plot(r,z, "bx")
+ r =          3.9086
+ z =          7.7297
+ pylab.plot(r,z, "bx")
+ r =          3.9654
+ z =          7.7983
+ pylab.plot(r,z, "bx")
+ r =          2.4960
+ z =          6.2954
+ pylab.plot(r,z, "bx")
+ r =          2.5804
+ z =          6.3077
+ pylab.plot(r,z, "bx")
+ r =          2.6575
+ z =          6.3489
+ pylab.plot(r,z, "bx")
+ r =          2.7292
+ z =          6.4037
+ pylab.plot(r,z, "bx")
+ r =          2.7972
+ z =          6.4636
+ pylab.plot(r,z, "bx")
+ r =          2.8636
+ z =          6.5262
+ pylab.plot(r,z, "bx")
+ r =          2.9291
+ z =          6.5903
+ pylab.plot(r,z, "bx")
+ r =          2.9935
+ z =          6.6557
+ pylab.plot(r,z, "bx")
+ r =          3.0559
+ z =          6.7202
+ pylab.plot(r,z, "bx")
+ r =          3.1180
+ z =          6.7858
+ pylab.plot(r,z, "bx")
+ r =          3.1793
+ z =          6.8517
+ pylab.plot(r,z, "bx")
+ r =          3.2397
+ z =          6.9179
+ pylab.plot(r,z, "bx")
+ r =          3.2995
+ z =          6.9844
+ pylab.plot(r,z, "bx")
+ r =          3.3583
+ z =          7.0510
+ pylab.plot(r,z, "bx")
+ r =          3.4165
+ z =          7.1179
+ pylab.plot(r,z, "bx")
+ r =          3.4741
+ z =          7.1851
+ pylab.plot(r,z, "bx")
+ r =          3.5311
+ z =          7.2525
+ pylab.plot(r,z, "bx")
+ r =          3.5874
+ z =          7.3200
+ pylab.plot(r,z, "bx")
+ r =          3.6434
+ z =          7.3879
+ pylab.plot(r,z, "bx")
+ r =          3.6992
+ z =          7.4561
+ pylab.plot(r,z, "bx")
+ r =          3.7546
+ z =          7.5240
+ pylab.plot(r,z, "bx")
+ r =          3.8104
+ z =          7.5928
+ pylab.plot(r,z, "bx")
+ r =          3.8662
+ z =          7.6613
+ pylab.plot(r,z, "bx")
+ r =          3.9220
+ z =          7.7295
+ pylab.plot(r,z, "bx")
+ r =          3.9790
+ z =          7.7982
+ pylab.plot(r,z, "bx")
+ r =          2.5465
+ z =          6.1169
+ pylab.plot(r,z, "bx")
+ r =          2.6122
+ z =          6.1987
+ pylab.plot(r,z, "bx")
+ r =          2.6796
+ z =          6.2768
+ pylab.plot(r,z, "bx")
+ r =          2.7444
+ z =          6.3494
+ pylab.plot(r,z, "bx")
+ r =          2.8087
+ z =          6.4201
+ pylab.plot(r,z, "bx")
+ r =          2.8740
+ z =          6.4913
+ pylab.plot(r,z, "bx")
+ r =          2.9392
+ z =          6.5621
+ pylab.plot(r,z, "bx")
+ r =          3.0038
+ z =          6.6325
+ pylab.plot(r,z, "bx")
+ r =          3.0659
+ z =          6.7005
+ pylab.plot(r,z, "bx")
+ r =          3.1284
+ z =          6.7696
+ pylab.plot(r,z, "bx")
+ r =          3.1901
+ z =          6.8384
+ pylab.plot(r,z, "bx")
+ r =          3.2511
+ z =          6.9071
+ pylab.plot(r,z, "bx")
+ r =          3.3112
+ z =          6.9758
+ pylab.plot(r,z, "bx")
+ r =          3.3703
+ z =          7.0441
+ pylab.plot(r,z, "bx")
+ r =          3.4288
+ z =          7.1125
+ pylab.plot(r,z, "bx")
+ r =          3.4865
+ z =          7.1809
+ pylab.plot(r,z, "bx")
+ r =          3.5436
+ z =          7.2494
+ pylab.plot(r,z, "bx")
+ r =          3.6001
+ z =          7.3178
+ pylab.plot(r,z, "bx")
+ r =          3.6561
+ z =          7.3862
+ pylab.plot(r,z, "bx")
+ r =          3.7120
+ z =          7.4551
+ pylab.plot(r,z, "bx")
+ r =          3.7673
+ z =          7.5233
+ pylab.plot(r,z, "bx")
+ r =          3.8235
+ z =          7.5927
+ pylab.plot(r,z, "bx")
+ r =          3.8787
+ z =          7.6608
+ pylab.plot(r,z, "bx")
+ r =          3.9349
+ z =          7.7295
+ pylab.plot(r,z, "bx")
+ r =          3.9920
+ z =          7.7982
+ pylab.plot(r,z, "bx")
+ r =          2.7432
+ z =          6.1723
+ pylab.plot(r,z, "bx")
+ r =          2.7651
+ z =          6.2450
+ pylab.plot(r,z, "bx")
+ r =          2.8027
+ z =          6.3168
+ pylab.plot(r,z, "bx")
+ r =          2.8489
+ z =          6.3867
+ pylab.plot(r,z, "bx")
+ r =          2.8999
+ z =          6.4549
+ pylab.plot(r,z, "bx")
+ r =          2.9549
+ z =          6.5244
+ pylab.plot(r,z, "bx")
+ r =          3.0109
+ z =          6.5922
+ pylab.plot(r,z, "bx")
+ r =          3.0676
+ z =          6.6598
+ pylab.plot(r,z, "bx")
+ r =          3.1241
+ z =          6.7262
+ pylab.plot(r,z, "bx")
+ r =          3.1812
+ z =          6.7932
+ pylab.plot(r,z, "bx")
+ r =          3.2383
+ z =          6.8601
+ pylab.plot(r,z, "bx")
+ r =          3.2952
+ z =          6.9270
+ pylab.plot(r,z, "bx")
+ r =          3.3518
+ z =          6.9940
+ pylab.plot(r,z, "bx")
+ r =          3.4077
+ z =          7.0606
+ pylab.plot(r,z, "bx")
+ r =          3.4633
+ z =          7.1274
+ pylab.plot(r,z, "bx")
+ r =          3.5184
+ z =          7.1943
+ pylab.plot(r,z, "bx")
+ r =          3.5733
+ z =          7.2613
+ pylab.plot(r,z, "bx")
+ r =          3.6277
+ z =          7.3283
+ pylab.plot(r,z, "bx")
+ r =          3.6818
+ z =          7.3952
+ pylab.plot(r,z, "bx")
+ r =          3.7358
+ z =          7.4623
+ pylab.plot(r,z, "bx")
+ r =          3.7896
+ z =          7.5291
+ pylab.plot(r,z, "bx")
+ r =          3.8443
+ z =          7.5971
+ pylab.plot(r,z, "bx")
+ r =          3.8983
+ z =          7.6638
+ pylab.plot(r,z, "bx")
+ r =          3.9534
+ z =          7.7310
+ pylab.plot(r,z, "bx")
+ r =          4.0093
+ z =          7.7983
+ pylab.plot(r,z, "bx")
+ r =          2.8282
+ z =          6.1964
+ pylab.plot(r,z, "bx")
+ r =          2.8508
+ z =          6.2731
+ pylab.plot(r,z, "bx")
+ r =          2.8853
+ z =          6.3465
+ pylab.plot(r,z, "bx")
+ r =          2.9271
+ z =          6.4172
+ pylab.plot(r,z, "bx")
+ r =          2.9733
+ z =          6.4858
+ pylab.plot(r,z, "bx")
+ r =          3.0233
+ z =          6.5545
+ pylab.plot(r,z, "bx")
+ r =          3.0739
+ z =          6.6205
+ pylab.plot(r,z, "bx")
+ r =          3.1261
+ z =          6.6867
+ pylab.plot(r,z, "bx")
+ r =          3.1785
+ z =          6.7519
+ pylab.plot(r,z, "bx")
+ r =          3.2318
+ z =          6.8173
+ pylab.plot(r,z, "bx")
+ r =          3.2851
+ z =          6.8824
+ pylab.plot(r,z, "bx")
+ r =          3.3386
+ z =          6.9477
+ pylab.plot(r,z, "bx")
+ r =          3.3920
+ z =          7.0131
+ pylab.plot(r,z, "bx")
+ r =          3.4451
+ z =          7.0781
+ pylab.plot(r,z, "bx")
+ r =          3.4981
+ z =          7.1433
+ pylab.plot(r,z, "bx")
+ r =          3.5508
+ z =          7.2086
+ pylab.plot(r,z, "bx")
+ r =          3.6035
+ z =          7.2740
+ pylab.plot(r,z, "bx")
+ r =          3.6560
+ z =          7.3395
+ pylab.plot(r,z, "bx")
+ r =          3.7082
+ z =          7.4048
+ pylab.plot(r,z, "bx")
+ r =          3.7603
+ z =          7.4701
+ pylab.plot(r,z, "bx")
+ r =          3.8124
+ z =          7.5354
+ pylab.plot(r,z, "bx")
+ r =          3.8657
+ z =          7.6017
+ pylab.plot(r,z, "bx")
+ r =          3.9186
+ z =          7.6671
+ pylab.plot(r,z, "bx")
+ r =          3.9723
+ z =          7.7326
+ pylab.plot(r,z, "bx")
+ r =          4.0271
+ z =          7.7983
+ pylab.plot(r,z, "bx")
+ r =          2.8965
+ z =          6.2157
+ pylab.plot(r,z, "bx")
+ r =          2.9198
+ z =          6.2954
+ pylab.plot(r,z, "bx")
+ r =          2.9532
+ z =          6.3709
+ pylab.plot(r,z, "bx")
+ r =          2.9929
+ z =          6.4429
+ pylab.plot(r,z, "bx")
+ r =          3.0367
+ z =          6.5123
+ pylab.plot(r,z, "bx")
+ r =          3.0839
+ z =          6.5812
+ pylab.plot(r,z, "bx")
+ r =          3.1308
+ z =          6.6461
+ pylab.plot(r,z, "bx")
+ r =          3.1798
+ z =          6.7114
+ pylab.plot(r,z, "bx")
+ r =          3.2294
+ z =          6.7759
+ pylab.plot(r,z, "bx")
+ r =          3.2796
+ z =          6.8401
+ pylab.plot(r,z, "bx")
+ r =          3.3299
+ z =          6.9039
+ pylab.plot(r,z, "bx")
+ r =          3.3807
+ z =          6.9678
+ pylab.plot(r,z, "bx")
+ r =          3.4315
+ z =          7.0318
+ pylab.plot(r,z, "bx")
+ r =          3.4821
+ z =          7.0954
+ pylab.plot(r,z, "bx")
+ r =          3.5329
+ z =          7.1592
+ pylab.plot(r,z, "bx")
+ r =          3.5835
+ z =          7.2230
+ pylab.plot(r,z, "bx")
+ r =          3.6342
+ z =          7.2870
+ pylab.plot(r,z, "bx")
+ r =          3.6848
+ z =          7.3510
+ pylab.plot(r,z, "bx")
+ r =          3.7352
+ z =          7.4147
+ pylab.plot(r,z, "bx")
+ r =          3.7854
+ z =          7.4782
+ pylab.plot(r,z, "bx")
+ r =          3.8360
+ z =          7.5418
+ pylab.plot(r,z, "bx")
+ r =          3.8878
+ z =          7.6065
+ pylab.plot(r,z, "bx")
+ r =          3.9396
+ z =          7.6706
+ pylab.plot(r,z, "bx")
+ r =          3.9919
+ z =          7.7344
+ pylab.plot(r,z, "bx")
+ r =          4.0455
+ z =          7.7983
+ pylab.plot(r,z, "bx")
+ pylab.axis("equal")
+ pylab.show()
+ 
+main()
